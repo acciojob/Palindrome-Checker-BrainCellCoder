9aad3d17afa6e5538a6855be8fa4cfe8e1d8e41d
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 11:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(s){
+	const cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
 }
 module.exports = palindrome
